80d04f83c9f4139f3c069276aa4453efbc40562e
ismailtgr <ismailtegar1@gmail.com>
2026-05-28 23:00:21 +0700

    penambangan middleware role

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -8,13 +8,20 @@ use Symfony\Component\HttpFoundation\Response;
 
 class RoleMiddleware
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  Closure(Request): (Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
+    public function handle(
+        Request $request,
+        Closure $next,
+        string $role
+    ): Response {
+
+        if (!$request->user()) {
+            abort(401);
+        }
+
+        if ($request->user()->role !== $role) {
+            abort(403);
+        }
+
         return $next($request);
     }
 }
